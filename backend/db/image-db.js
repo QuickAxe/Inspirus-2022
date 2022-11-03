27e0267493eaef9115fb5286b027a854676b3575
TheInvincible95 <109205375+TheInvincible95@users.noreply.github.com>
2022-11-04 01:35:16 +0530

    function to add images to Cloud Storage and DB

---
+++ backend/db/image-db.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const Users = require('../models/Users');
+const Images = require('../models/Images');
+
+
+
+module.exports = async function addToDB(_id,prompt) {
+    
+
+};
