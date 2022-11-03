834ab45c656a50814e030bd28afd4bc982c28b9d
TheInvincible95 <109205375+TheInvincible95@users.noreply.github.com>
2022-11-03 15:22:55 +0530

    Set up Mongoose/MongoDB

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,24 @@
 //import 
 const express = require('express');
 const app = express();
-port = 3000;
-
+const mongoose = require('mongoose')
+const port = 3000;
+require('dotenv/config')
 app.get('/', (req, res) => {
     res.send('Hello world');
 }
 );
 
+
+// connecting to MongoDB
+
 app.listen(port, () => {
     console.log(`Listen on ${port}`);
 });
+mongoose.connect(
+    process.env.DB_CONNECTION,
+    { useNewUrlParser: true},
+    () => console.log('Connected to MongoDB!')
+);
+    
 module.exports = app;
