e2d981b83b9b2bf1109a95ae8234d2864e347a7a
ManselInfinity <106764539+ManselInfinity@users.noreply.github.com>
2022-11-03 17:59:24 +0530

    added lots of stuff

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,3 @@
-//import express from 'express'
-//import 
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
@@ -34,4 +32,4 @@ mongoose.connect(
     () => console.log('Connected to MongoDB!')
 );
     
-module.exports = app;
+//module.exports = app;
