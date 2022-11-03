b206a1972d40ab3043989bc895accf1d6a1518ac
ManselInfinity <106764539+ManselInfinity@users.noreply.github.com>
2022-11-04 01:34:06 +0530

    hi

--- backend/routes/sd.js
+++ backend/routes/sd.js
@@ -9,6 +9,10 @@ router.post('/', async (req, res) => {
     const prompt = req.body;
     await sd(prompt.theme, prompt.des, prompt.color);
     //res.send(user);
+   /*
+   database stuff prompt userid
+   */
+
     res.send(200);
 });
 
