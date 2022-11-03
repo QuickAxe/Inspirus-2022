9f8ba99c93cb1f9ca1cfd8272970121e4dd07a36
ManselInfinity <106764539+ManselInfinity@users.noreply.github.com>
2022-11-03 14:36:28 +0530

    Init

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,15 @@
-import express from 'express'
+//import express from 'express'
 //import 
+const express = require('express');
+const app = express();
+port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello world');
+}
+);
+
+app.listen(port, () => {
+    console.log(`Listen on ${port}`);
+});
+module.exports = app;
