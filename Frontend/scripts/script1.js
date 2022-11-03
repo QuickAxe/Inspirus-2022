242c1fc974c3ef0447ef77e2077e51bf96d4b657
atharvaayir <atharvaayir@gmail.com>
2022-11-03 20:34:58 +0530

    Made the landing page

---
+++ Frontend/scripts/script1.js
@@ -0,0 +1,9 @@
+function f1()
+{
+    let str1=document.getElementById("text1").value;
+    alert(str1);
+    let str2=document.getElementById("text2").value;
+    alert(str2);
+    let str3=document.getElementById("text3").value;
+    alert(str3);
+}
