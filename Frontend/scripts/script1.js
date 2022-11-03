af4a299b9071f09bad97a9af01586035a85ad4c6
atharvaayir <atharvaayir@gmail.com>
2022-11-03 23:09:39 +0530

    Added...

--- Frontend/scripts/script1.js
+++ Frontend/scripts/script1.js
@@ -1,9 +1,21 @@
 function f1()
 {
-    let str1=document.getElementById("text1").value;
-    alert(str1);
-    let str2=document.getElementById("text2").value;
-    alert(str2);
-    let str3=document.getElementById("text3").value;
-    alert(str3);
+     str1=document.getElementById("text1").value;
+    str2=document.getElementById("text2").value;
+   str3=document.getElementById("text3").value;
+// alert(theo);
+
+data = {
+        "theme":str1,
+        "des":str2,
+        "color":str3
+    }
+console.log(data);
+
+str=JSON.stringify(data);
+
+
+
+
+
 }
